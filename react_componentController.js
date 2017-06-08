c74caefafae9437e7e9de3f2c833064a2b72e326
Himanshu Tanwar <himanshucubed@gmail.com>
2017-06-08 12:05:03 +0530

    Create react_componentController.js

--- react_componentController.js
+++ react_componentController.js
@@ -14,7 +14,7 @@ var ComponentController = function () {
     var BaseComponent = function (_React$Component) {
         _inherits(BaseComponent, _React$Component);
 
-        function BaseComponent(props) {
+        function BaseComponent() {
             _classCallCheck(this, BaseComponent);
 
             return _possibleConstructorReturn(this, (BaseComponent.__proto__ || Object.getPrototypeOf(BaseComponent)).call(this));
@@ -27,6 +27,9 @@ var ComponentController = function () {
                 var controller = new this.props.init(this);
                 this.controller = controller;
                 this.setState(this.props.state);
+                if (this.props.callback) {
+                    this.props.callback(controller);
+                }
             }
         }, {
             key: "render",
@@ -44,29 +47,92 @@ var ComponentController = function () {
 
     ;
 
+    function setItemFromObjectKey(obj, key, item) {
+        var key = key.split(".");
+        var data = obj;
+        var prop;
+        for (var i = 1; i < key.length - 1; i++) {
+            prop = key[i];
+            data = data[prop];
+        }
+        data[key.pop()] = item;
+        return obj;
+    }
+
     function getBaseController() {
-        function Controller(context) {
-            this.component = context;
+        function Controller(component) {
             var that = this;
+            this.childKeys = "root";
             this.action = function (name) {
                 var args = Array.prototype.slice.call(arguments);
                 args.shift();
                 var fn = Function.prototype.bind.apply(that[name], [that].concat(args));
                 return fn;
             };
+            this.setState = function (obj, key) {
+                if (key) {
+                    var data = setItemFromObjectKey(component.state, key, obj);
+                    data = JSON.parse(JSON.stringify(data));
+                    component.setState(data);
+                } else {
+                    component.setState(obj);
+                }
+            };
+            this.getState = function () {
+                return component.state;
+            };
         }
         return Controller;
     }
 
-    function render(Component, container, props, state) {
+    function getChildController(parent, child) {
+        function Controller() {
+            var that = this;
+            var key = child;
+            this.action = function (name) {
+                var args = Array.prototype.slice.call(arguments);
+                args.shift();
+                var fn = Function.prototype.bind.apply(that[name], [that].concat(args));
+                return fn;
+            };
+            this.setState = function (obj) {
+                var keyList = parent.props.controller.childKeys;
+                parent.props.controller.setState(obj, [keyList, key].join("."));
+            };
+        }
+        return Controller;
+    }
+
+    function render(Component, container, props, state, callback) {
         var Controller = getBaseController();
         var properties = controllers[Component.name] || {};
 
-        ReactDOM.render(React.createElement(BaseComponent, { init: Controller,
+        var baseComponent = React.createElement(BaseComponent, { init: Controller,
+            callback: callback,
             comp: Component,
             props: props,
             properties: properties || {},
-            state: state }), container);
+            state: state });
+
+        if (container) {
+            ReactDOM.render(baseComponent, container);
+        } else {
+            return baseComponent;
+        }
+    }
+
+    function component(Comp, props, state, parent, child) {
+        var Controller = parent && child ? getChildController(parent, child) : getBaseController();
+        var properties = controllers[Comp.name] || {};
+
+        var baseComponent = React.createElement(BaseComponent, { init: Controller,
+            comp: Comp,
+            key: props.key,
+            props: props,
+            properties: properties || {},
+            state: state });
+
+        return baseComponent;
     }
 
     function create(component, controller) {
@@ -76,6 +142,7 @@ var ComponentController = function () {
 
     return {
         render: render,
+        component: component,
         create: create
     };
 }();
