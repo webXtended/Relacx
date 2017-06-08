356361664ca2ded88f0d9b10db4ac29b7e3ffc58
Himanshu Tanwar <himanshucubed@gmail.com>
2017-06-08 12:04:01 +0530

    Create react_componentController.jsx
    add support for inner components

--- react_componentController.jsx
+++ react_componentController.jsx
@@ -1,8 +1,7 @@
 var ComponentController = (function () {
     var controllers = {};
-
     class BaseComponent extends React.Component{
-        constructor(props){
+        constructor(){
             super();
         }
         componentWillMount() {
@@ -10,6 +9,9 @@ var ComponentController = (function () {
             var controller = new this.props.init(this);
             this.controller = controller;
             this.setState(this.props.state);
+            if(this.props.callback){
+                this.props.callback(controller);
+            }
         }
         render() {
             var Element = this.props.comp;
@@ -22,32 +24,95 @@ var ComponentController = (function () {
         }
     };
 
+    function setItemFromObjectKey(obj,key,item){
+        var key = key.split(".");
+        var data = obj;
+        var prop;
+        for(var i=1;i<key.length-1;i++){
+                prop = key[i];
+                data=data[prop];
+        }
+        data[key.pop()] = item;
+        return obj;
+    }
+
     function getBaseController() {
-        function Controller(context) {
-            this.component = context;
+        function Controller(component) {
             var that = this;
+            this.childKeys="root";
             this.action = function(name){
                 var args = Array.prototype.slice.call(arguments);
                 args.shift();
                 var fn = Function.prototype.bind.apply(that[name],[that].concat(args));
                 return fn;
+            };
+            this.setState = function(obj,key){
+                if(key){
+                    var data = setItemFromObjectKey(component.state,key, obj);
+                    data = JSON.parse(JSON.stringify(data));
+                    component.setState(data);
+                }else{
+                    component.setState(obj);
+                }
+            };
+            this.getState = function(){
+                return component.state;
             }
         }
         return Controller;
     }
 
-    function render(Component, container, props, state) {
+    function getChildController(parent, child){
+        function Controller() {
+            var that = this;
+            var key = child;
+            this.action = function(name){
+                var args = Array.prototype.slice.call(arguments);
+                args.shift();
+                var fn = Function.prototype.bind.apply(that[name],[that].concat(args));
+                return fn;
+            };
+            this.setState = function(obj){
+                var keyList = parent.props.controller.childKeys;
+                parent.props.controller.setState(obj, [keyList,key].join("."));
+            };
+        }
+        return Controller;
+    }
+
+
+    function render(Component, container, props, state, callback) {
         var Controller = getBaseController();
         var properties = controllers[Component.name]  || {};
 
-        ReactDOM.render(
-            <BaseComponent init={Controller}
-                           comp={Component}
-                           props={props}
-                           properties={properties || {}}
-                           state={state}>
-            </BaseComponent>,
-            container);
+        var baseComponent =  <BaseComponent init={Controller}
+                                            callback={callback}
+                                            comp={Component}
+                                            props={props}
+                                            properties={properties || {}}
+                                            state={state}>
+                            </BaseComponent>
+
+        if(container){
+            ReactDOM.render(baseComponent,container);
+        }else{
+            return baseComponent;
+        }
+    }
+
+    function component(Comp, props, state, parent, child){
+        var Controller = parent&&child?getChildController(parent, child):getBaseController();
+        var properties = controllers[Comp.name]  || {};
+
+        var baseComponent =  <BaseComponent init={Controller}
+                                            comp={Comp}
+                                            key={props.key}
+                                            props={props}
+                                            properties={properties || {}}
+                                            state={state}>
+                             </BaseComponent>
+
+        return baseComponent;
     }
 
     function create(component, controller) {
@@ -57,6 +122,8 @@ var ComponentController = (function () {
 
     return {
         render: render,
+        component: component,
         create: create
     }
 })();
+
